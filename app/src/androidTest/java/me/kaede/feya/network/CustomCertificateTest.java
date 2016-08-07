6243517b10e9033e0c1d5b608c4171ca0b2d8418
Kaede <kidhaibara@gmail.com>
2016-08-07 20:55:11 +0800

    update ssl test

--- app/src/androidTest/java/me/kaede/feya/network/CustomCertificateTest.java
+++ app/src/androidTest/java/me/kaede/feya/network/CustomCertificateTest.java
@@ -2,6 +2,7 @@ package me.kaede.feya.network;
 
 import android.content.Context;
 import android.test.InstrumentationTestCase;
+import android.text.TextUtils;
 import android.util.Log;
 
 import javax.net.ssl.*;
@@ -37,9 +38,26 @@ public class CustomCertificateTest extends InstrumentationTestCase {
 
     public void testGetKeyStore() {
         try {
+            //// TODO: 16/8/7 is there a way to access device central keystore
+            Log.i(TAG, "Android Central KeyStore");
             KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
             keyStore.load(null);
             Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                String item = aliases.nextElement();
+                Log.i(TAG, "keystore alias = " + item);
+            }
+
+            Log.i(TAG, "Custom KeyStore");
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            // uwca.crt 打包在 asset 中，该证书可以从https://itconnect.uw.edu/security/securing-computer/install/safari-os-x/下载
+            InputStream caInput = new BufferedInputStream(mContext.getAssets().open("uwca.crt"));
+            Certificate ca = cf.generateCertificate(caInput);
+            Log.i(TAG, "ca=" + ((X509Certificate) ca).getSubjectDN());
+            Log.i(TAG, "key=" + ca.getPublicKey());
+            caInput.close();
+            keyStore.setCertificateEntry("ca", ca);
+            aliases = keyStore.aliases();
             while (aliases.hasMoreElements()) {
                 String item = aliases.nextElement();
                 Log.i(TAG, "keystore alias = " + item);
@@ -53,7 +71,6 @@ public class CustomCertificateTest extends InstrumentationTestCase {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     /**
@@ -61,7 +78,7 @@ public class CustomCertificateTest extends InstrumentationTestCase {
      * use built-in certificates with empty TrustManager (no verifying)
      * work, but this is unsafe, the client will not will check server's validity
      */
-    public void testCustomCer1() {
+    public void testHttpsWithEmptyVerifying() {
         String url = "https://certs.cac.washington.edu/CAtest/";
         String html = null;
         InputStream mInputStream = null;
@@ -75,15 +92,9 @@ public class CustomCertificateTest extends InstrumentationTestCase {
                 }
 
                 public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                    for (X509Certificate item : chain) {
-                        Log.d(TAG, "certificate = " + item.toString());
-                    }
                 }
 
                 public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                    for (X509Certificate item : chain) {
-                        Log.d(TAG, "certificate = " + item.toString());
-                    }
                 }
             }};
             SSLContext sslContext = SSLContext.getInstance("TLS");
@@ -140,18 +151,34 @@ public class CustomCertificateTest extends InstrumentationTestCase {
         Log.d(TAG, "url content = " + html);
     }
 
+
+    public void testCustomCertificate1() {
+        String url = "https://certs.cac.washington.edu/CAtest/";
+        String html = doHttpsWithCustomCertificate(url);
+        assertNotNull(html);
+        Log.d(TAG, "url content = " + html);
+    }
+
+    public void testCustomCertificate2() {
+        String url = "https://www.google.com";
+        String html = doHttpsWithCustomCertificate(url);
+        assertNotNull(html);
+        Log.d(TAG, "url content = " + html);
+    }
+
     /**
      * use https url connection
      * custom certificate with default TrustManager (default algorithm)
      */
-    public void testCustomCer2() {
-        String url = "https://www.google.com";
+    private String doHttpsWithCustomCertificate(String url) {
+        assertTrue(!TextUtils.isEmpty(url));
         String html = null;
         InputStream mInputStream = null;
         ByteArrayOutputStream mByteArrayOutputStream = null;
         HttpsURLConnection connection = null;
-
         try {
+            URL mUrl = new URL(url);
+            connection = (HttpsURLConnection) mUrl.openConnection();
             // custom keystore
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
             // uwca.crt 打包在 asset 中，该证书可以从https://itconnect.uw.edu/security/securing-computer/install/safari-os-x/下载
@@ -174,10 +201,7 @@ public class CustomCertificateTest extends InstrumentationTestCase {
             // 3. Create an SSLContext that uses our TrustManager
             SSLContext sslContext = SSLContext.getInstance("TLSv1", "AndroidOpenSSL");
             sslContext.init(null, tmf.getTrustManagers(), null);
-            // 4. create https url connection
-            URL mUrl = new URL(url);
-            connection = (HttpsURLConnection) mUrl.openConnection();
-            // 5. ssl socket factory
+            // 4. set ssl socket factory for the connection
             // or use HttpsURLConnection#setDefaultSSLSocketFactory for all connection
             connection.setSSLSocketFactory(sslContext.getSocketFactory());
             connection.setRequestMethod("GET");
@@ -211,9 +235,7 @@ public class CustomCertificateTest extends InstrumentationTestCase {
             if (connection != null) {
                 connection.disconnect();
             }
-
         }
-        assertNotNull(html);
-        Log.d(TAG, "url content = " + html);
+        return html;
     }
 }
