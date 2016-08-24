ce672af0c35f12d5d5b3980b6abd8cf2073f738d
Kaede <kidhaibara@gmail.com>
2016-08-24 23:36:29 +0800

    [modify] bytes io

--- app/src/main/java/me/kaede/feya/protobuff/ProtobuffActivity.java
+++ app/src/main/java/me/kaede/feya/protobuff/ProtobuffActivity.java
@@ -6,6 +6,7 @@
 package me.kaede.feya.protobuff;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,11 @@ import android.widget.Button;
 import com.squareup.dinosaurs.Dinosaur;
 import com.squareup.geology.Period;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import butterknife.BindView;
@@ -29,8 +35,6 @@ public class ProtobuffActivity extends BaseActivity implements View.OnClickListe
     Button btn1;
     @BindView(R.id.btn2)
     Button btn2;
-
-    private byte[] stegosaurusBytes = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,20 +58,42 @@ public class ProtobuffActivity extends BaseActivity implements View.OnClickListe
                 toast("dinosaur name = " + stegosaurus.name + ", existed in the "
                         + stegosaurus.period + " period.");
 
-                stegosaurusBytes = Dinosaur.ADAPTER.encode(stegosaurus);
-                String byteContent =  new String(stegosaurusBytes);
+                byte[] stegosaurusBytes = Dinosaur.ADAPTER.encode(stegosaurus);
+                String byteContent = new String(stegosaurusBytes);
                 Log.d(TAG, "encoded byte content = " + byteContent);
                 toast("encoded byte content = " + byteContent);
+
+                Log.d(TAG, "write encoded bytes to file");
+                BufferedOutputStream bos = null;
+                try {
+                    bos = new BufferedOutputStream(new FileOutputStream(
+                            Environment.getExternalStorageDirectory() + File.separator + "dinosaur_byte"));
+                    bos.write(stegosaurusBytes);
+                    bos.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bos != null) {
+                        try {
+                            bos.close();
+                        } catch (IOException ignore) {
+                        }
+                    }
+                }
+
                 break;
 
             case R.id.btn2:
-                if (stegosaurusBytes == null) {
+                File file = new File(Environment.getExternalStorageDirectory() + File.separator + "dinosaur_byte");
+                if (!file.exists()) {
                     Log.d(TAG, "pls create byte content first.");
                     toast("pls create byte content first.");
                     return;
                 }
                 try {
-                    Dinosaur tyrannosaurus = Dinosaur.ADAPTER.decode(stegosaurusBytes);
+                    Log.d(TAG, "read encoded bytes from file.");
+                    byte[] bytes = FileUtils.readFileToByteArray(file);
+                    Dinosaur tyrannosaurus = Dinosaur.ADAPTER.decode(bytes);
                     Log.d(TAG, "dinosaur name = " + tyrannosaurus.name + ", existed in the "
                             + tyrannosaurus.period + " period.");
                     toast("dinosaur name = " + tyrannosaurus.name + ", existed in the "
