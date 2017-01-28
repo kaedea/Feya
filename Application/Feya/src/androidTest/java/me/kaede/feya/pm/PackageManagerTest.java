0044b1baa30676d6c26b951c2a94bfae47f44505
Kaede <kidhaibara@gmail.com>
2017-01-28 19:16:07 +0800

    update PM test

--- Application/Feya/src/androidTest/java/me/kaede/feya/pm/PackageManagerTest.java
+++ Application/Feya/src/androidTest/java/me/kaede/feya/pm/PackageManagerTest.java
@@ -12,6 +12,9 @@ import android.content.pm.PackageManager;
 import android.test.InstrumentationTestCase;
 import android.util.Log;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -43,27 +46,33 @@ public class PackageManagerTest extends InstrumentationTestCase {
         }
     }
 
-    public void testGetInstallPackageInfo() {
+    public void testGetInstallPackageInfo() throws NoSuchFieldException, IllegalAccessException {
         PackageManager pm = mContext.getPackageManager();
-        List<PackageInfo> packages = pm.getInstalledPackages(PackageManager.GET_ACTIVITIES);
+        List<PackageInfo> packages = pm.getInstalledPackages(0);
         assertNotNull(packages);
 
         Log.d(TAG, "all package num = " + packages.size());
         for (PackageInfo packageInfo : packages) {
             Log.i(TAG, "-");
-            try {
-                // PackageInfo
-                String packageName = packageInfo.packageName;
-                Log.d(TAG, "packageName = " + packageName);
-                // Package's ApplicationInfo
-                ApplicationInfo applicationInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-                Log.d(TAG, "publicSourceDir = " + applicationInfo.publicSourceDir);
-                boolean isSystemApp = (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
-                Log.w(TAG, "is system app = " + isSystemApp);
-                Log.d(TAG, "application info = " + applicationInfo.toString());
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
+            // PackageInfo
+            Log.v(TAG, "packageName = " + packageInfo.packageName);
+            Log.v(TAG, "version name = " + packageInfo.versionName);
+            Log.v(TAG, "version code = " + packageInfo.versionCode);
+
+            Field field = findField(packageInfo, "coreApp");
+            boolean coreApp = (boolean) field.get(packageInfo);
+            Log.v(TAG, "coreApp = " + coreApp);
+
+            Log.i(TAG, "Package info = " + packageInfo.toString());
+
+            // Package's ApplicationInfo
+            ApplicationInfo applicationInfo = packageInfo.applicationInfo;
+            Log.d(TAG, "publicSourceDir = " + applicationInfo.publicSourceDir);
+            boolean isSystemApp = (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+            Log.d(TAG, "Is system app = " + isSystemApp);
+            Log.d(TAG, "Is enabled = " + applicationInfo.enabled);
+
+            Log.i(TAG, "application info = " + applicationInfo.toString());
             Log.d(TAG, "package info =" + packageInfo.toString());
             Log.i(TAG, "-");
         }
@@ -90,6 +99,53 @@ public class PackageManagerTest extends InstrumentationTestCase {
         }
         Log.d(TAG, "package info =" + packageInfo.toString());
         Log.i(TAG, "-");
+    }
 
+    /**
+     * Locates a given field anywhere in the class inheritance hierarchy.
+     *
+     * @param instance an object to search the field into.
+     * @param name field name
+     * @return a field object
+     * @throws NoSuchFieldException if the field cannot be located
+     */
+    private static Field findField(Object instance, String name) throws NoSuchFieldException {
+        for (Class<?> clazz = instance.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
+            try {
+                Field field = clazz.getDeclaredField(name);
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+                return field;
+            } catch (NoSuchFieldException e) {
+                // ignore and search next
+            }
+        }
+        throw new NoSuchFieldException("Field " + name + " not found in " + instance.getClass());
+    }
+    /**
+     * Locates a given method anywhere in the class inheritance hierarchy.
+     *
+     * @param instance an object to search the method into.
+     * @param name method name
+     * @param parameterTypes method parameter types
+     * @return a method object
+     * @throws NoSuchMethodException if the method cannot be located
+     */
+    private static Method findMethod(Object instance, String name, Class<?>... parameterTypes)
+            throws NoSuchMethodException {
+        for (Class<?> clazz = instance.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
+            try {
+                Method method = clazz.getDeclaredMethod(name, parameterTypes);
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+                return method;
+            } catch (NoSuchMethodException e) {
+                // ignore and search next
+            }
+        }
+        throw new NoSuchMethodException("Method " + name + " with parameters " +
+                Arrays.asList(parameterTypes) + " not found in " + instance.getClass());
     }
 }
