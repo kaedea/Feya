fab40b8de835cc48fed79f9bc1e4f239d554022d
Kaede <kidhaibara@gmail.com>
2017-03-28 21:03:35 +0800

    update suck.js

--- Script/NodeJs/simpleServer.js
+++ Script/NodeJs/simpleServer.js
@@ -1,54 +1,72 @@
-var express = require('express');
-var AdmZip = require('adm-zip');
-var fs = require('fs'),  request = require('request');
-var md5File = require('md5-file')
+const express = require('express');
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+const request = require('request');
+const md5File = require('md5-file');
+const lineReader = require('line-reader');
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    // console.log('content-type:', res.headers['content-type']);
-    // console.log('content-length:', res.headers['content-length']);
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
+const download = function(uri, filename, callback) {
+    request.head(uri, function(err, res, body) {
+        // console.log('content-type:', res.headers['content-type']);
+        // console.log('content-length:', res.headers['content-length']);
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
 };
 
+// Temp dir.
+const tempDir = './temp';
+try {
+    fs.accessSync(tempDir);
+} catch (e) {
+    fs.mkdirSync(tempDir);
+}
 
-var app = express();
-app.get('/suck', function(req, res){
-  var apkUrl = req.query.apkUrl;
-  console.log('apk url = ' + apkUrl);
+const app = express();
+const port = 3000;
+app.get('/suck', function(req, res) {
+    var apkUrl = req.query.apkUrl;
+    console.log('apk url = ' + apkUrl);
 
-  if (apkUrl.endsWith('.apk')) {
-    console.log('download apk...');
-    // Download apk.
-    download(apkUrl, './temp/apks/temp.apk', function(){
-        console.log('done');
+    if (apkUrl.startsWith('http')) {
+        console.log('download file...');
+        // Download apk.
+        var apkFile = tempDir + '/temp.apk';
+        download(apkUrl, apkFile, function() {
+            console.log('done');
 
-        var apkId = 'Unspecific';
-        var stats = fs.statSync('./temp/apks/temp.apk')
-        var fileSize = stats.size
-        var md5 = md5File.sync('./temp/apks/temp.apk')
+            var apkId = 'Unspecific';
+            var stats = fs.statSync(apkFile)
+            var fileSize = stats.size
+            var md5 = md5File.sync(apkFile)
 
-        // Extract.
-        var zip = new AdmZip("./temp/apks/temp.apk");
-        zip.extractEntryTo("META-INF/CERT.SF", "./temp", false, true);
+            // Extract.
+            var zip = new AdmZip(apkFile);
+            zip.extractEntryTo("META-INF/CERT.SF", tempDir, false, true);
+            var symbol = 'SHA1-Digest-Manifest:'
+            lineReader.eachLine(tempDir + '/CERT.SF', function(line, last) {
+                if (line.indexOf(symbol) != -1) {
+                    apkId = line.substring(line.indexOf(symbol) + symbol.length);
+                    console.log('manifest id = ' + apkId)
 
-        var lineReader = require('line-reader');
-        var symbol = 'SHA1-Digest-Manifest:'
-        lineReader.eachLine('./temp/CERT.SF', function(line, last) {
-            if (line.indexOf(symbol) !=-1) {
-              apkId = line.substring(line.indexOf(symbol) + symbol.length);
-              console.log('manifest id = ' + apkId)
-
-              // Output.
-              res.send('APK_ID = ' + apkId.trim() +
-                      '<br> APK_MD5 = ' + md5 +
-                      '<br> APK_SIZE = ' + fileSize +
-                      '<br> APK_URL = ' + apkUrl)
-              return false;
-            }
+                    // Output.
+                    res.writeHead(200, {
+                        "Content-Type": "application/json"
+                    });
+                    var json = JSON.stringify({
+                        apk_id: apkId.trim(),
+                        apk_md5: md5,
+                        apk_size: fileSize,
+                        apk_url: apkUrl
+                    });
+                    res.end(json);
+                    return false;
+                }
+            });
         });
-    });
-  }
+    } else {
+        console.log('apk url is invalid!');
+        res.end('apk url is invalid!' + '<br> APK_URL = ' + apkUrl);
+    }
 });
 
-app.listen(3000);
+app.listen(port);
