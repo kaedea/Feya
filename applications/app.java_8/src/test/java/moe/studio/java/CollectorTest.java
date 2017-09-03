0632eb734907c1ef45cda7e1c3e25d312493fcd9
Kaede <kidhaibara@gmail.com>
2017-09-03 21:04:00 +0800

    Update Collector test snippet.

--- applications/app.java_8/src/test/java/moe/studio/java/CollectorTest.java
+++ applications/app.java_8/src/test/java/moe/studio/java/CollectorTest.java
@@ -11,9 +11,12 @@ import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeSet;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -25,15 +28,17 @@ import java.util.stream.Stream;
  */
 @RunWith(JUnit4.class)
 public class CollectorTest {
+    private static final List<Integer> INT_ARRAYS = Arrays.asList(1, 2, 3, 4);
+    private static final List<String> TEXT_ARRAYS = Arrays.asList("hello", "functional", "coding");
 
     @Test
     public void testToCollection() {
         Supplier<ArrayList> listSupplier = ArrayList::new;
         //noinspection unchecked
         List<Integer> list = Stream
-                .of(1,2,3,4)
+                .of(1, 2, 3, 4)
                 .collect(Collectors.toCollection(listSupplier));
-        Assert.assertEquals(list, Arrays.asList(1,2,3,4));
+        Assert.assertEquals(list, Arrays.asList(1, 2, 3, 4));
 
         HashSet<Integer> hashSet = Stream
                 .of(1, 2, 3, 3)
@@ -54,5 +59,85 @@ public class CollectorTest {
         Assert.assertEquals(map.get(1), "1");
         Assert.assertEquals(map.get(2), "2");
         Assert.assertEquals(map.getOrDefault(0, "default"), "default");
+    }
+
+    @Test
+    public void testCalculating() {
+        Optional<Integer> maxOptional = INT_ARRAYS.stream().collect(
+                Collectors.maxBy(Integer::compareTo)
+        );
+        Assert.assertTrue(maxOptional.isPresent());
+        Assert.assertEquals(maxOptional.get().intValue(), 4);
+
+        Optional<Integer> minOptional = INT_ARRAYS.stream().collect(
+                Collectors.minBy(Integer::compareTo)
+        );
+        Assert.assertTrue(minOptional.isPresent());
+        Assert.assertEquals(minOptional.get().intValue(), 1);
+
+        IntSummaryStatistics summary = INT_ARRAYS.stream().collect(
+                Collectors.summarizingInt(value -> value)
+        );
+        Assert.assertEquals(summary.getCount(), 4);
+        Assert.assertEquals(summary.getSum(), 1 + 2 + 3 + 4);
+    }
+
+    @Test
+    public void testPartitioning() {
+        Map<Boolean, List<Integer>> oddMap = INT_ARRAYS.stream().collect(
+                Collectors.partitioningBy(x -> (x % 2) == 1)
+        );
+        Assert.assertEquals(oddMap.get(true), Arrays.asList(1, 3));
+        Assert.assertEquals(oddMap.get(false), Arrays.asList(2, 4));
+    }
+
+    @Test
+    public void testGrouping() {
+        Map<Integer, List<Integer>> intMap = INT_ARRAYS.stream().collect(
+                Collectors.groupingBy(Integer::new)
+        );
+        Assert.assertEquals(intMap.get(1), Collections.singletonList(1));
+        Assert.assertEquals(intMap.get(2), Collections.singletonList(2));
+        Assert.assertEquals(intMap.keySet().size(), 4);
+    }
+
+    @Test
+    public void testStringJoining() {
+        String combine = TEXT_ARRAYS.stream().collect(Collectors.joining());
+        Assert.assertEquals(combine, "hello" + "functional" + "coding");
+
+        combine = TEXT_ARRAYS.stream().collect(
+                Collectors.joining(",", "[", "]")
+        );
+        Assert.assertEquals(combine, "[hello,functional,coding]");
+    }
+
+    @Test
+    public void testMapping() {
+        Map<Boolean, List<String>> oddMap = INT_ARRAYS.stream().collect(Collectors.groupingBy(
+                integer -> integer % 2 == 1,
+                Collectors.mapping(Object::toString, Collectors.toList())
+        ));
+        Assert.assertEquals(oddMap.keySet().size(), 2);
+        Assert.assertEquals(oddMap.get(true), Arrays.asList("1", "3"));
+        Assert.assertEquals(oddMap.get(false), Arrays.asList("2", "4"));
+    }
+
+    @Test
+    public void testReducing() {
+        Integer acc = INT_ARRAYS.stream().collect(Collectors.reducing(0, (x, y) -> x + y));
+        //noinspection PointlessArithmeticExpression
+        Assert.assertEquals(acc.intValue(), 0 + 1 + 2 + 3 + 4);
+
+        Integer max = INT_ARRAYS.stream().collect(Collectors.reducing(
+                0,
+                integer -> integer,
+                (x, y) -> {
+                    if (x > y) return x;
+                    return y;
+                }
+
+        ));
+        Assert.assertEquals(max.intValue(), 4);
     }
 }
