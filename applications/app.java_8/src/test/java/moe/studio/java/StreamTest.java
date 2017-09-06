833c7b065c02bd46bf2eb05912c92f5c70047136
Kaede <kidhaibara@gmail.com>
2017-09-07 00:51:25 +0800

    Update Stream operations test snippet.

--- applications/app.java_8/src/test/java/moe/studio/java/StreamTest.java
+++ applications/app.java_8/src/test/java/moe/studio/java/StreamTest.java
@@ -11,14 +11,18 @@ import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.Random;
 import java.util.Set;
 import java.util.Spliterator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -75,6 +79,25 @@ public class StreamTest {
                 Arrays.asList("1", "2", "3", "4", "hello", "functional", "coding"),
                 concatStream.collect(Collectors.toList())
         );
+        // Stream#builder
+        Stream.Builder<Integer> builder = Stream.builder();
+        builder.accept(1);
+        List<Integer> builderList = builder.add(2).add(3).add(4).build().collect(
+                Collectors.toList()
+        );
+        Assert.assertEquals(Arrays.asList(1, 2, 3, 4), builderList);
+
+        // IntStream#range, rangeClosed
+        // for (int i = startInclusive; i < endExclusive ; i++)
+        List<Integer> rangeList = IntStream.range(1, 5).boxed().collect(Collectors.toList());
+        Assert.assertEquals(Arrays.asList(1, 2, 3, 4), rangeList);
+        // for (int i = startInclusive; i <- endExclusive ; i++)
+        List<Integer> rangeClosedList = IntStream.rangeClosed(1, 4).boxed().collect(Collectors.toList());
+        Assert.assertEquals(Arrays.asList(1, 2, 3, 4), rangeClosedList);
+
+        // Random#ints
+        long size = new Random().ints().limit(3).count();
+        Assert.assertEquals(3, size);
     }
 
     @Test
@@ -128,6 +151,13 @@ public class StreamTest {
                 .collect(Collectors.toList());
         Assert.assertEquals(Arrays.asList(1, 2, 3, 4), sortedList);
         Assert.assertEquals(Arrays.asList(4, 3, 2, 1), reverseSortedList);
+
+        // IntStream#boxed
+        // Array to list
+        List<Integer> boxedList = Arrays.stream(new int[]{1, 2, 3, 4})
+                .boxed()
+                .collect(Collectors.toList());
+        Assert.assertEquals(Arrays.asList(1, 2, 3, 4), boxedList);
     }
 
     @Test
@@ -176,7 +206,29 @@ public class StreamTest {
         intList = new ArrayList<>();
         Assert.assertTrue(spliterator.tryAdvance(intList::add));
         spliterator.forEachRemaining(intList::add);
-        Assert.assertEquals(Arrays.asList(1, 2, 3, 4), intList);
+
+        // IntStream#sum
+        int intSum = INT_ARRAYS.stream().mapToInt(Integer::intValue).sum();
+        Assert.assertEquals(1 + 2 + 3 + 4, intSum);
+        // IntStream#asDoubleStream, asLongStream
+        OptionalDouble optionalDouble = INT_ARRAYS.stream()
+                .mapToInt(Integer::intValue)
+                .asDoubleStream()
+                .average();
+        Assert.assertTrue(optionalDouble.isPresent());
+        Assert.assertEquals(
+                0,
+                Double.compare((double) (1 + 2 + 3 + 4) / 4, optionalDouble.getAsDouble())
+        );
+        // IntStream#summaryStatistics
+        IntSummaryStatistics intSummary = INT_ARRAYS.stream()
+                .mapToInt(Integer::intValue)
+                .summaryStatistics();
+        Assert.assertEquals(4, intSummary.getCount());
+        Assert.assertEquals(4, intSummary.getMax());
+        Assert.assertEquals(1, intSummary.getMin());
+        Assert.assertEquals(1 + 2 + 3 + 4, intSummary.getSum());
+        Assert.assertEquals(0, Double.compare((double) (1 + 2 + 3 + 4) / 4, intSummary.getAverage()));
     }
 
     @Test
@@ -228,30 +280,104 @@ public class StreamTest {
 
     @Test
     public void testParallel() {
-        List<Integer> integerList = Stream.of(1, 2, 3, 4)
+        // Stream#parallel
+        int oddSum = Stream.of(1, 2, 3, 4)
                 .parallel()
                 .filter(integer -> integer % 2 == 1)
-                .collect(Collectors.toList());
-        Assert.assertEquals(integerList, Arrays.asList(1, 3));
-
-        integerList = INT_ARRAYS.parallelStream()
-                .filter(integer -> integer > 1)
-                .collect(Collectors.toList());
-        Assert.assertEquals(integerList, Arrays.asList(2, 3, 4));
-
+                .mapToInt(Integer::intValue)
+                .sum();
+        Assert.assertEquals(1 + 3, oddSum);
+        // Collection#parallelStream
+        int evenSum = INT_ARRAYS.parallelStream()
+                .filter(integer -> integer % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        Assert.assertEquals(2 + 4, evenSum);
+        // Arrays#parallelSetAll, parallelPrefix
         int[] ints = new int[4];
         Arrays.parallelSetAll(ints, operand -> operand);
         Assert.assertEquals(
                 Arrays.stream(ints).boxed().collect(Collectors.toList()),
                 Arrays.asList(0, 1, 2, 3)
         );
-
         Integer[] integers = new Integer[4];
         Arrays.parallelSetAll(integers, operand -> operand);
         Arrays.parallelPrefix(integers, (integer1, integer2) -> integer1 + integer2);
         Assert.assertEquals(
                 new ArrayList<>(Arrays.asList(integers)),
                 Arrays.asList(0, 1, 3, 6)
+        );
+
+        // Stream#sequential
+        List<Integer> INT_ARRAYS = Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+        );
+        List<Integer> defaultIterateList = new ArrayList<>();
+        List<Integer> parallelIterateList = new ArrayList<>();
+        List<Integer> sequentialIterateList = new ArrayList<>();
+
+        List<Integer> collectDefaultList = INT_ARRAYS.stream()
+                .peek(it -> {
+                    System.out.println("Consuming " + it);
+                    defaultIterateList.add(it);
+                })
+                .collect(Collectors.toList());
+        Assert.assertEquals(Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30), defaultIterateList);
+        Assert.assertEquals(Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30), collectDefaultList);
+
+        System.out.println("");
+        List<Integer> collectParallelList = INT_ARRAYS.stream()
+                .parallel()
+                .peek(it -> {
+                    System.out.println("Consuming " + it);
+                    parallelIterateList.add(it);
+                })
+                .collect(Collectors.toList());
+        Assert.assertNotEquals(Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30), parallelIterateList);
+        Assert.assertEquals(Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30), collectParallelList);
+
+        System.out.println("");
+        List<Integer> collectSequentialList = INT_ARRAYS.stream()
+                .parallel()
+                .sequential()
+                .peek(it -> {
+                    System.out.println("Consuming " + it);
+                    sequentialIterateList.add(it);
+                })
+                .collect(Collectors.toList());
+        Assert.assertEquals(Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30), sequentialIterateList);
+        Assert.assertEquals(Arrays.asList(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30), collectSequentialList);
+
+        // Stream#unordered
+        // Unordered operations does not guarantee disordering the stream.
+        //
+        // If a stream is ordered, repeated execution of identical stream pipelines on an identical
+        // source will produce an identical result; if it is not ordered, repeated execution might
+        // produce different results. For parallel streams, relaxing the ordering constraint can
+        // sometimes enable more efficient execution.
+        //
+        // In cases where the stream has an encounter order, but the user does not particularly care
+        // about that encounter order, explicitly de-ordering the stream with unordered() may improve
+        // parallel performance for some stateful or terminal operations.
+        System.out.println("");
+        Stream.of(
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+        ).unordered().peek(it -> System.out.println("Consuming " + it)).collect(
+                Collectors.toList()
         );
     }
 }
