7c898d986263646a959685eca16f831ab57504ca
Kaede <kidhaibara@gmail.com>
2018-08-28 19:24:09 +0800

    Add kotlin concurrent test

--- feya/applications/app.kotlin/src/test/kotlin/com/kaedea/kotlin/KotlinConcurrentTest.kt
+++ feya/applications/app.kotlin/src/test/kotlin/com/kaedea/kotlin/KotlinConcurrentTest.kt
@@ -3,6 +3,8 @@ package com.kaedea.kotlin
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.test.assertEquals
 
@@ -17,11 +19,12 @@ class KtConcurrentTest {
     @Test
     fun waitNotify() {
         val lock = Object()
-        var text : String? = null
+        var text: String? = null
 
         Thread {
             Thread.sleep(100)
             text = "2233"
+
             synchronized(lock) {
                 lock.notifyAll()
             }
@@ -37,10 +40,43 @@ class KtConcurrentTest {
     }
 
     @Test
+    fun waitNotify2() {
+        fun Object.await(block: () -> Unit) {
+            synchronized(this) {
+                wait()
+            }
+            block()
+        }
+
+        fun Object.signal() {
+            synchronized(this) {
+                notify()
+            }
+        }
+
+        val lock = Object()
+        var text: String? = null
+
+        Thread {
+            Thread.sleep(100)
+            text = "2233"
+
+            lock.signal()
+        }.start()
+
+        lock.await {
+            assertEquals("2233", text)
+            text = "3322"
+        }
+        assertEquals("3322", text)
+    }
+
+
+    @Test
     fun lockCondition() {
         val lock = ReentrantLock()
         val condition = lock.newCondition()
-        var text : String? = null
+        var text: String? = null
 
         Thread {
             Thread.sleep(100)
@@ -64,5 +100,44 @@ class KtConcurrentTest {
         }
 
         assertEquals("2233", text)
+    }
+
+    @Test
+    fun lockCondition2() {
+        fun Lock.wait(condition: Condition, block: () -> Unit) {
+            lock()
+            try {
+                condition.await()
+            } finally {
+                unlock()
+            }
+            block()
+        }
+
+        fun Lock.notify(condition: Condition) {
+            lock()
+            try {
+                condition.signal()
+            } finally {
+                unlock()
+            }
+        }
+
+        val lock = ReentrantLock()
+        val condition = lock.newCondition()
+        var text: String? = null
+
+        Thread {
+            Thread.sleep(100)
+            text = "2233"
+
+            lock.notify(condition)
+        }.start()
+
+        lock.wait(condition) {
+            assertEquals("2233", text)
+            text = "3322"
+        }
+        assertEquals("3322", text)
     }
 }
